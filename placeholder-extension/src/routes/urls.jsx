a79db54af416148f23238efedd3b5bedf65da504
AtlasWiki <nathanwong3421@gmail.com>
2024-08-18 22:05:00 -0700

    add more wireframe components to #urls route

--- placeholder-extension/src/routes/urls.jsx
+++ placeholder-extension/src/routes/urls.jsx
@@ -3,8 +3,85 @@ export function URLS(){
     return(
         <div>
             <NavBar />
-            <h1>URLS</h1>
-            <p>
+            <h1 className="mb-1">URLS</h1>
+            <div className="py-1 bg-gray-950/20 overflow-y-auto max-h-40">
+                <table className="w-full">
+                    <tr>
+                        <th>Endpoint</th>
+                        <th>Location</th>
+                        <th>Extension</th>
+                        <th>Code Snippet</th>
+                    </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                   <tr>
+                        <td>/admin</td>
+                        <td>{ window.location.pathname }</td>
+                        <td>path</td>
+                        <a href="#" target="_blank">View here</a>
+                   </tr>
+                </table>
+            </div>
+            <div className="m-4">
+                <button>Download as TXT</button>
+                <button>Download as JSON</button>
+                <button>Copy as absolute URLs</button>
+                <button>Copy All</button>
+            </div>
+          
+            <p className="mt-2">
                 Located at  { document.location.href }
             </p>
         </div>
