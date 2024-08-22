9639639b0774cd5f850ce0cc6ee1cde9a79945c3
LordCat <kristianalexball@gmail.com>
2024-08-23 00:29:26 +0100

    Working button states

--- public/background.js
+++ public/background.js
@@ -1,11 +1,31 @@
+
+//initliaze the button state on start up
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({
+    urlParser: false,
+    fileDownloader: false
+  }, () => {
+    console.log("Default values set on installation");
+  });
+});
+
+
+//Managed the state of the buttons
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log('Background script received message:', message);
-  chrome.storage.local.set({ urlParser: message }).then(() => {
-    console.log("Cache has been set");
-  });
-  chrome.storage.local.get(["urlParser"]).then((result) => {
-    console.log("Cache is " + result.urlParser);
-  });
-  sendResponse({ response: result.urlParser });
+
+  if (message.action === 'urlParserChanged') {
+    chrome.storage.local.set({ urlParser: message.state }, () => {
+      console.log("URL Parser state saved:", message.state);
+    });
+  } else if (message.action === 'fileDownloaderChanged') {
+    chrome.storage.local.set({ fileDownloader: message.state }, () => {
+      console.log("File Downloader state saved:", message.state);
+    });
+  }
+
+  // Send the response back to the pop up 
+  sendResponse({ success: true });
 });
+
 
