7a8e56e244e89ff5e7c8b67be9d086c9207870be
AtlasWiki <nathanwong3421@gmail.com>
2024-08-21 17:24:14 -0700

    add visit section in urls

--- src/PopUp/routes/urls.tsx
+++ src/PopUp/routes/urls.tsx
@@ -5,63 +5,63 @@ export function URLS(){
             <NavBar />
             <h1 className="mb-1">URLS</h1>
             <div className="py-1 bg-gray-950/20 overflow-y-auto max-h-40">
-                <table className="w-full">
+               <table className="w-full">
+                         <tr>
+                         <th>Endpoint</th>
+                         <th>Location</th>
+                         <th>Code Snippet</th>
+                         </tr>
                     <tr>
-                        <th>Endpoint</th>
-                        <th>Location</th>
-                        <th>Code Snippet</th>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
                     </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                   <tr>
-                        <td>/admin</td>
-                        <td>{ window.location.pathname }</td>
-                        <a href="#" target="_blank">View here</a>
-                   </tr>
-                </table>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+                    <tr>
+                         <td>/admin</td>
+                         <td>{ window.location.pathname }</td>
+                         <a href="#" target="_blank">View here</a>
+                    </tr>
+               </table>
             </div>
             <div className="m-4">
                 <button className="m-0.5">Download as TXT</button>
@@ -69,8 +69,18 @@ export function URLS(){
                 <button className="m-0.5">Copy as absolute URLs</button>
                 <button className="m-0.5">Copy All</button>
             </div>
-          
-            <p className="mt-2">
+
+            <h1 className="mb-1">Visited</h1>
+            <div className="py-1 bg-gray-950/20 overflow-y-auto max-h-40">
+               <ul className="flex flex-col gap-3">
+                    <li>{document.location.href}</li>
+                    <li>{document.location.href}</li>
+                    <li>{document.location.href}</li>
+                    <li>{document.location.href}</li>
+               </ul>
+            </div>
+
+            <p className="mt-5">
                 Located at  { document.location.href }
             </p>
         </div>
