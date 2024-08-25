c4d4f410322e55347fdb3b873fb658227c693cfe
AtlasWiki <nathanwong3421@gmail.com>
2024-08-25 14:45:16 -0700

    fix table spacing, sizing, and positioning errors on urls.tsx

--- src/DevTool/routes/urls.tsx
+++ src/DevTool/routes/urls.tsx
@@ -1,68 +1,68 @@
-import { NavBar } from '../../components/navbar'
-import "../App.css"
-import "../index.css"
+import { NavBar } from '../../components/navbar';
+import "../App.css";
+import "../index.css";
 
-export function URLS(){
-    return(
+export function URLS() {
+    return (
         <div className="w-full min-h-screen">
             <NavBar />
             <div className="mt-5 flex flex-col w-full gap-5">
-               <div className="w-full mb-5"> 
+                <div className="w-full mb-5 px-5"> 
                     <h1 className="text-2xl">Location:</h1>
                     <select className="mt-5 p-5 bg-gray-500/80 text-lg w-full">
-                         <option value="all">ALL</option>
-                         <option value={document.location.href}>{document.location.href}</option>
-                         <option value={document.location.href}>{document.location.href}</option>
-                         <option value={document.location.pathname}>{document.location.pathname}</option>
+                        <option value="all">ALL</option>
+                        <option value={document.location.href}>{document.location.href}</option>
+                        <option value={document.location.href}>{document.location.href}</option>
+                        <option value={document.location.pathname}>{document.location.pathname}</option>
                     </select>
-               </div>
-               <div className="py-1 w-full">
-                    <table className="w-full">
-                         <tr className="text-4xl mb-20">
-                              <th className="border-b-2">Endpoint</th>
-                              <th className="border-b-2">Location</th>
-                              <th className="border-b-2">Linked</th>
-                              <th className="border-b-2">Code</th>
-                         </tr>
-                         <div className="mt-20"></div>
-                         {/* <tr>
-                              <td><input className="p-2 pb-3 text-lg rounded-md bg-gray-500/80" type="text"/></td>
-                         </tr> */}
-                         <tr className="">
-                              <td>/admin</td>
-                              <td>{ window.location.href }</td>
-                              <td>{ window.location.host + window.location.pathname } </td>
-                              <td><a href="#" target="_blank">View here</a></td>
-                         </tr>
-                         <tr className="">
-                              <td>/admin</td>
-                              <td>{ window.location.href }</td>
-                              <td>{ window.location.host + window.location.pathname } </td>
-                              <td><a href="#" target="_blank">View here</a></td>
-                         </tr>
-                         <tr className="">
-                              <td>/admin</td>
-                              <td>{ window.location.href }</td>
-                              <td>{ window.location.host + window.location.pathname } </td>
-                              <td><a href="#" target="_blank">View here</a></td>
-                         </tr>
-                         <tr className="">
-                              <td>/admin</td>
-                              <td>{ window.location.href }</td>
-                              <td>{ window.location.host + window.location.pathname } </td>
-                              <td><a href="#" target="_blank">View here</a></td>
-                         </tr>
+                </div>
+                <div className="py-1 w-full flex flex-col gap-10">
+                    <table className="w-full mb-5 border-collapse">
+                        <thead>
+                            <tr className="text-3xl">
+                                <th className="border-b-2 pb-10">ENDPOINT</th>
+                                <th className="border-b-2 pb-10">LOCATION</th>
+                                <th className="border-b-2 pb-10">LINKED</th>
+                                <th className="border-b-2 pb-10">CODE</th>
+                            </tr>
+                        </thead>
+                        <div className="mt-20"></div>
+                        <tbody>
+                            <tr>
+                                <td>/admin</td>
+                                <td>{window.location.href}</td>
+                                <td>{window.location.host + window.location.pathname}</td>
+                                <td><a href="#" target="_blank">View here</a></td>
+                            </tr>
+                            <tr>
+                                <td>/admin</td>
+                                <td>{window.location.href}</td>
+                                <td>{window.location.host + window.location.pathname}</td>
+                                <td><a href="#" target="_blank">View here</a></td>
+                            </tr>
+                            <tr>
+                                <td>/admin</td>
+                                <td>{window.location.href}</td>
+                                <td>{window.location.host + window.location.pathname}</td>
+                                <td><a href="#" target="_blank">View here</a></td>
+                            </tr>
+                            <tr>
+                                <td>/admin</td>
+                                <td>{window.location.href}</td>
+                                <td>{window.location.host + window.location.pathname}</td>
+                                <td><a href="#" target="_blank">View here</a></td>
+                            </tr>
+                        </tbody>
                     </table>
-                    <div className="mt-5 text-lg flex justify-content items-center">
-                         <a href={document.location.href} target="_blank" className="m-2 bg-gray-950 p-3 rounded-md">Open in New Tab</a>
-                         <button className="m-0.5">Download as TXT</button>
-                         <button className="m-0.5">Download as JSON</button>
-                         <button className="m-0.5">Copy as absolute URLs</button>
-                         <button className="m-0.5">Copy All</button>
+                    <div className="text-lg flex items-center space-x-4 px-5">
+                        <a href={document.location.href} target="_blank" className="bg-gray-950 p-3 rounded-md">Open in New Tab</a>
+                        <button className="bg-gray-800 p-3 rounded-md">Download as TXT</button>
+                        <button className="bg-gray-800 p-3 rounded-md">Download as JSON</button>
+                        <button className="bg-gray-800 p-3 rounded-md">Copy as absolute URLs</button>
+                        <button className="bg-gray-800 p-3 rounded-md">Copy All</button>
                     </div>
-               </div>
-              
+                </div>
             </div>
         </div>
-    )
+    );
 }
