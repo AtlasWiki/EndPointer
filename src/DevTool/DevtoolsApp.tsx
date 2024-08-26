ffe707ff937e7ea8fe5d2a48d12c07cd01c53007
AtlasWiki <nathanwong3421@gmail.com>
2024-08-26 02:20:00 -0700

    add page reload when clearing cache in devtoolsapp.tsx

--- src/DevTool/DevtoolsApp.tsx
+++ src/DevTool/DevtoolsApp.tsx
@@ -36,7 +36,7 @@ function DevToolsApp() {
         <button onClick={() => location.reload()}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#4d4c4c" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M19.933 13.041a8 8 0 1 1-9.925-8.788c3.899-1 7.935 1.007 9.425 4.747"/><path d="M20 4v5h-5"/></g></svg>
         </button>
-        <button onClick={() => {clearCache();alert("cache cleared")}}>
+        <button onClick={() => {clearCache();alert("cache cleared");location.reload()}}>
           Clear Cache
         </button>
       </div>
