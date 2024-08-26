55e56de4d51636e528fb13254a842ed889e0712d
AtlasWiki <nathanwong3421@gmail.com>
2024-08-25 20:22:27 -0700

    make on/off state for URL parsing

--- src/DevTool/content.js
+++ src/DevTool/content.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("loaded from content script: " + document.location.href);
-    alert("from DOMContentLoaded");
-});
-alert(document.location.href);
-prompt("DevTools");
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("loaded from content script: " + document.location.href);
+//     alert("from DOMContentLoaded");
+// });
+// alert(document.location.href);
+// prompt("DevTools");
