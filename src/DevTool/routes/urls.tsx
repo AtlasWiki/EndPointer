a8ad06e4a5b0ec22a9d4d7347bb3a466f1188109
AtlasWiki <nathanwong3421@gmail.com>
2024-08-30 23:04:45 -0700

    correct of navbar items in devtools urls.tsx

--- src/DevTool/routes/urls.tsx
+++ src/DevTool/routes/urls.tsx
@@ -125,11 +125,13 @@ export function URLS() {
      window.location.reload()
     }
   return (
-    <div className="ml-2 mt-2 w-full min-h-screen">
-      <NavBar />
-      <button className="mt-2" onClick={() => location.reload()}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#4d4c4c" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M19.933 13.041a8 8 0 1 1-9.925-8.788c3.899-1 7.935 1.007 9.425 4.747"/><path d="M20 4v5h-5"/></g></svg>
-      </button>
+    <div className="w-full min-h-screen">
+      <div className="ml-2 mt-2">
+        <NavBar />
+        <button className="mt-2" onClick={() => location.reload()}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#4d4c4c" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M19.933 13.041a8 8 0 1 1-9.925-8.788c3.899-1 7.935 1.007 9.425 4.747"/><path d="M20 4v5h-5"/></g></svg>
+        </button>
+      </div>
       <div className="mt-5 flex">
         <div className="py-1 w-full flex flex-col gap-10">
           <div className="w-full max-h-[760px] overflow-auto">
