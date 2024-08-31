22575eb36d003b28dd9cc7c4e90e9e1961277a39
AtlasWiki <nathanwong3421@gmail.com>
2024-08-30 22:42:28 -0700

    replace clear urls to trashcan icon in urls.tsx

--- src/PopUp/routes/urls.tsx
+++ src/PopUp/routes/urls.tsx
@@ -181,7 +181,9 @@ export function URLs() {
           </div>
           <div className="text-lg flex items-center space-x-4 px-5">
             <a href={document.location.origin + "/PopUp/popup.html#urls"} target="_blank" className="bg-gray-950 p-3 rounded-md">Open in New Tab</a>
-            <button className="bg-gray-600 p-3 rounded-md" onClick={clearURLs}>Clear URLs</button>
+            <button className="bg-gray-600 p-3 rounded-md" onClick={clearURLs}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"/></svg>
+            </button>
             <button className="bg-gray-600 p-3 rounded-md">Download as TXT</button>
             <button className="bg-gray-600 p-3 rounded-md">Download as JSON</button>
             <button className="bg-gray-600 p-3 rounded-md">Copy as absolute URLs</button>
