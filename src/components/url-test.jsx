dc4022833d2a2a1751483f453a73ba911db5a805
AtlasWiki <nathanwong3421@gmail.com>
2024-09-03 15:23:53 -0700

    clean up files

--- src/components/url-test.jsx
+++
@@ -1,9 +0,0 @@
-export function URLTest(){
-    return(
-        <div>
-            {urls.map((url, index) => (
-                URLItems(url, webpage, jsFile)
-            ))}
-        </div>
-    )
-}
