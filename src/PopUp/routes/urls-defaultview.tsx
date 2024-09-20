39202b98fe2fa676cdcb2df0b77d8af83897aeab
AtlasWiki <nathanwong3421@gmail.com>
2024-09-20 15:48:09 -0700

    adjust spacing and add urls-plain url button

--- src/PopUp/routes/urls-defaultview.tsx
+++ src/PopUp/routes/urls-defaultview.tsx
@@ -381,7 +381,7 @@ export function URLsDefaultView() {
     }
 
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-full min-h-screen flex justify-center item-center">
       <div className="mt-5 flex">
         <div className="py-1 w-full flex flex-col gap-10">
           <div className="w-full max-h-[760px] overflow-auto">
@@ -442,7 +442,7 @@ export function URLsDefaultView() {
             <button className="a-item bg-gray-600 p-3 rounded-md" onClick={clearURLs}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"/></svg>
             </button>
-            <button className="a-item bg-gray-600 p-3 rounded-md">Download as TXT</button>
+            <a href={document.location.origin + "/PopUp/popup.html#urls/plain"} target="_blank" className="a-item bg-gray-600 p-3 rounded-md">Download as TXT</a>
             <button className="a-item bg-gray-600 p-3 rounded-md">Download as JSON</button>
             <button className="a-item bg-gray-600 p-3 rounded-md">Copy as absolute URLs</button>
             <button className="a-item bg-gray-600 p-3 rounded-md">Copy All</button>
