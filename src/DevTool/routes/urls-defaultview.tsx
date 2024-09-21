4ee275bdf28829892580b40a87b1104f77c8594e
AtlasWiki <nathanwong3421@gmail.com>
2024-09-20 17:52:02 -0700

    change table names for default view

--- src/DevTool/routes/urls-defaultview.tsx
+++ src/DevTool/routes/urls-defaultview.tsx
@@ -390,8 +390,8 @@ export function URLsDefaultView() {
               <thead>
                 <tr className="text-5xl">
                   <th className="border-b-2 pb-10">ENDPOINT <span className="text-[#3da28f]">({filteredURLs.length})</span></th>
-                  <th className="border-b-2 pb-10">LOCATION <span className="text-[#3da28f]">({jsFiles.length})</span></th>
-                  <th className="border-b-2 pb-10">ROOT</th>
+                  <th className="border-b-2 pb-10">SOURCE <span className="text-[#3da28f]">({jsFiles.length})</span></th>
+                  <th className="border-b-2 pb-10">WEBPAGE</th>
                 </tr>
               </thead>
               <tbody>
