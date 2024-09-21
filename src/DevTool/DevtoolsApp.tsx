b63f83d7fc1271e5e69ca35c8a1cdcab30a79bfb
AtlasWiki <nathanwong3421@gmail.com>
2024-09-20 17:38:06 -0700

    disable other unused routes for devtools

--- src/DevTool/DevtoolsApp.tsx
+++ src/DevTool/DevtoolsApp.tsx
@@ -72,9 +72,9 @@ function DevToolsApp() {
           <h2 className="text-xl md:text-4xl">Endpoint parsing</h2>
           <div className="text-md flex gap-2">
               <Link className="a-item a-color font-semibold" to="urls"><span className="text-violet-500">URLs</span> ({urlCount})</Link>
-              <Link className="a-item a-color font-semibold" to="creds"><span className="text-rose-500">Creds</span> ({credCount})</Link>
+              {/* <Link className="a-item a-color font-semibold" to="creds"><span className="text-rose-500">Creds</span> ({credCount})</Link>
               <Link className="a-item a-color font-semibold" to="apikeys"><span className="text-emerald-500">API keys</span> ({apiKeyCount})</Link>
-              <Link className="a-item a-color font-semibold" to="urlsvisited"><span className="text-emerald-500">URLs Visited</span> ({apiKeyCount})</Link>
+              <Link className="a-item a-color font-semibold" to="urlsvisited"><span className="text-emerald-500">URLs Visited</span> ({apiKeyCount})</Link> */}
           </div>
         </div>
 
