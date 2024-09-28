11010a91a445f2f99e390231547e0d7813d1e184
AtlasWiki <nathanwong3421@gmail.com>
2024-09-28 14:31:04 -0700

    change bg color and button color of modals

--- src/constants/defaultview_contants.ts
+++ src/constants/defaultview_contants.ts
@@ -19,8 +19,8 @@ export const LOCAL_STORAGE_KEYS = {
 } as const;
 
 export const CSS_CLASSES = {
-  BUTTON: 'px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600',
+  BUTTON: 'px-4 py-2 bg-black text-white rounded hover:bg-blue-600',
   INPUT: 'px-2 border-2 border-gray-300 bg-transparent text-lg w-full pb-3 pt-3 rounded-md cursor-pointer text-gray-300 hover:border-gray-500 outline-none focus:border-gray-500 transition-all duration-400',
   MODAL_OVERLAY: 'fixed inset-0 flex items-center justify-center bg-black bg-opacity-50',
-  MODAL_CONTENT: 'bg-white p-5 rounded-lg shadow-lg',
+  MODAL_CONTENT: 'bg-[#363333] opacity-85 p-5 rounded-lg shadow-lg',
 } as const;
