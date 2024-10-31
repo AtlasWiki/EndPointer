ac5ea48b96cc4d5a2e2cf5624dd1ebaf61acf1f2
AtlasWiki <nathanwong3421@gmail.com>
2024-10-31 16:41:18 -0700

    add css design for filter toggle button

--- src/routes/urls-defaultview.tsx
+++ src/routes/urls-defaultview.tsx
@@ -97,8 +97,8 @@ export function URLsDefaultView() {
                             placeholder="Search endpoints..."
                           />
                         {/* Filter button */}
-                         <button className="bg-transparent border-2 border-customFont hover:border-gray-300 hover:border-2 py-2 px-4 rounded-md" onClick={() => {setFilterToggle(!filterToggle)}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#316E7D" stroke="#316E7D" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 3h16a1 1 0 0 1 1 1v1.586a1 1 0 0 1-.293.707l-6.414 6.414a1 1 0 0 0-.293.707v6.305a1 1 0 0 1-1.242.97l-2-.5a1 1 0 0 1-.758-.97v-5.805a1 1 0 0 0-.293-.707L3.293 6.293A1 1 0 0 1 3 5.586V4a1 1 0 0 1 1-1"/></svg>
+                         <button className={`${ filterToggle ? 'bg-customFont'  : ""} border-2 border-customFont hover:border-gray-300 hover:border-2 py-2 px-4 rounded-md`} onClick={() => {setFilterToggle(!filterToggle)}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill={`${filterToggle ? '#141e24' : '#316E7D'}`} stroke={`${filterToggle ? '#141e24' : '#316E7D'}`} stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 3h16a1 1 0 0 1 1 1v1.586a1 1 0 0 1-.293.707l-6.414 6.414a1 1 0 0 0-.293.707v6.305a1 1 0 0 1-1.242.97l-2-.5a1 1 0 0 1-.758-.97v-5.805a1 1 0 0 0-.293-.707L3.293 6.293A1 1 0 0 1 3 5.586V4a1 1 0 0 1 1-1"/></svg>
                           </button>
                         </div>
                         {/* Filter menu */}
